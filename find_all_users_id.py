8ca3942a5a829124ace3aa5084114223675f5364
878 <majidovdiyorbek741@gmail.com>
2022-07-16 16:36:37 +0500

    data

--- find_all_users_id.py
+++ find_all_users_id.py
@@ -9,4 +9,17 @@ def find_all_users_id(data: dict)->list:
     Returns:
         list: List containing all the users id
     """
-    return
+    arr=[]
+    arr1=[]
+    for i in data["messages"] :
+        arr.append(i.get("from_id",0))
+        
+    for j in data['messages'] :
+        arr.append(i.get("actor_id",0))
+    for k in arr :
+        if k!=0 :
+            if k not in arr1 :
+                arr1.append(k)
+    return arr1[:-3]
+    
+print(find_all_users_id(read_data("data/result.json")))
--- find_number_of_messages.py
+++ find_number_of_messages.py
@@ -3,11 +3,11 @@ from read_data import read_data
 def find_number_of_messages(data: dict)->int:
     """
     Get the total number of messages.
-
     Parameters:
         data (dict): Dictionary containing the data of the json file.
     Returns:
         int: Total number of messages.
     
     """
-    return
+    return len(data["messages"])
+print(find_number_of_messages(read_data("data/result.json")))
